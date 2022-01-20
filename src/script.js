38cf5f66c62a869ca11228c6338482a4ae62e267
Emmanuel Etti <emmanueletti@gmail.com>
2022-01-19 21:10:01 -0800

    completes project

--- src/script.js
+++ src/script.js
@@ -2,37 +2,55 @@
 const button = document.getElementById('button');
 const audio = document.getElementById('audio');
 
-// test = () => {
-//   VoiceRSS.speech({
-//     key: '3fe402a15b524fb7be857b9a08902468',
-//     src: 'Hello World',
-//     hl: 'en-us',
-//     r: 0,
-//     c: 'mp3',
-//     f: '44khz_16bit_stereo',
-//     ssml: false,
-//   });
-// };
+const toggleButton = (buttonElement) => {
+  buttonElement.disabled = !buttonElement.disabled;
+};
 
-// // test();
-
-// Get jokes from Joke API
-async function getJokes() {
-  const API_URL =
-    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+// Turn joke text into speech
+async function turnTextToSpeech(text) {
   try {
-    const resp = await fetch(API_URL);
-    const data = await resp.json();
-    let joke;
-    if (data.type === 'twopart') {
-      joke = `${data.setup} ... ${data.delivery}`;
-    } else {
-      joke = data.joke;
-    }
-    console.log(joke);
+    VoiceRSS.speech({
+      key: '3fe402a15b524fb7be857b9a08902468',
+      src: text,
+      hl: 'en-us',
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false,
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
-// getJokes();
+// Get jokes from Joke API
+function getJoke() {
+  toggleButton(button);
+  const API_URL =
+    'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+
+  // Return a promise to make the getJoke function thenable
+  return fetch(API_URL)
+    .then((resp) => resp.json())
+    .then((data) => {
+      let joke;
+      if (data.type === 'twopart') {
+        joke = `${data.setup} ... ${data.delivery}`;
+      } else {
+        joke = data.joke;
+      }
+      return joke;
+    })
+    .catch((err) => {
+      console.log(err);
+      toggleButton(button);
+    });
+}
+
+const tellUserAJoke = () => {
+  getJoke().then((joke) => turnTextToSpeech(joke));
+};
+
+// Event listeners
+button.addEventListener('click', tellUserAJoke);
+audio.addEventListener('ended', () => toggleButton(button));
